9b35c9e345712461f6da926da3ac07e3e0f3d404
Woong <Parkwoong0131@gmail.com>
2023-02-27 18:53:50 +0900

    Add : folder step_6 and step_6 file

---
+++ step_6/090.py
@@ -0,0 +1,11 @@
+# 090
+# 다음 코드에서 에러가 발생한 원인을 설명하라.
+
+# >> icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+# >> icecream['누가바']
+# Traceback(most recent call last):
+#   File "<pyshell#69>", line 1, in <module >
+#   icecream['누가바']
+# KeyError: '누가바'
+
+# 누가바가 딕셔너리의 원소로 들어가있지않다.
